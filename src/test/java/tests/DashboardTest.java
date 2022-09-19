32cd11e460ccf5460692338cf6fdccb36153cdf2
p.lyahov <p.lyahov@a1qa.com>
2022-09-19 13:25:12 +0300

    Add suite and failed test

--- src/test/java/tests/DashboardTest.java
+++ src/test/java/tests/DashboardTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import steps.MainScreenSteps;
 import steps.ProductSteps;
@@ -16,6 +17,11 @@ public class DashboardTest extends BaseTest {
         MainScreenSteps.selectRandomCity();
         ProductSteps.verifyProductPriceWithDiscount();
         SellerSteps.verifySellerInfo();
+    }
+
+    @Test(description = "This test to check allure report")
+    public void testAllureReport() {
+        Assert.assertTrue(false, "Too see it in allure report");
     }
 
 }
